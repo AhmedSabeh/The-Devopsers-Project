09159e701f670c0432efcac1b4d5632e21260954
ahmedsabeh <ahmedsabeh911.com>
2025-12-08 10:02:26 +0200

    changing s3 bucket name

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "devopsers-s3-bucket"
+    bucket         = "devopsers-s3-bucket1"
     key            = "devopsers-statefile"
     region         = "us-east-1"
     encrypt        = true
